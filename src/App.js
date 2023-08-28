6d22ca16dcfacc792a3f37f0cf212146f5fa5025
Nihad <nihadalizadaxx@gmail.com>
2023-08-28 08:51:24 +0400

    improvement on React-Store-App-2

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import BasketList from './pages/BasketList';
+import FavoriteList from './pages/FavoriteList';
+import './App.scss';
+
+const App = () => {
+    const basketItems = useSelector(state => state.basket);
+    const favoriteItems = useSelector(state => state.favorite);
+
+    return (
+        <Router>
+            <div className="App">
+                <Header favoriteCount={favoriteItems.length} shoppingBasketCount={basketItems.length} />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/basket" element={<BasketList />} />
+                    <Route path="/favorite" element={<FavoriteList />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+App.propTypes = {
+    favoriteCount: PropTypes.number,
+    shoppingBasketCount: PropTypes.number,
+};
+
+export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProductsList from './ProductsList';
+import './main.scss';
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="main__content container">
+                <h1 className="main__content-title">
+                    WELCOME TO<span> GSTORE, </span>CHECK OUR LATEST games
+                </h1>
+                <div className="main__content-cards">
+                    <div className="main__content-cards__title-content">
+                        <h1 className="main__content-cards__title">
+                            LATEST ARRIVALS IN <span>G</span>STORE
+                        </h1>
+                    </div>
+                    <ProductsList />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Main.propTypes = {
+    onFavoriteChange: PropTypes.func,
+    onBasketChange: PropTypes.func,
+};
+
+export default Main;
